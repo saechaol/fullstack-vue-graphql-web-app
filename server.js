63f47b30711460642d3c376dfec543efd97ce2de
Lucas <33850575+saechaol@users.noreply.github.com>
2021-01-25 17:02:53 -0800

    Added a bit of documentation to code

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ const mongoose = require("mongoose");
 const fs = require("fs");
 const path = require("path");
 
+// Import type definitions and resolvers
 const filePath = path.join(__dirname, "typeDefs.gql");
 const typeDefs = fs.readFileSync(filePath, "utf-8");
 const resolvers = require("./resolvers");
 
+// Import environment variables and Mongoose models
 require("dotenv").config({ path: "variables.env" });
 const User = require("./models/User");
 const Post = require("./models/Post");
 
+// Connect to MLab database
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -21,7 +24,7 @@ mongoose
   .then(() => console.log("DB Connected"))
   .catch((err) => console.error(err));
 
-// Initialize ApolloServer object
+// Initialize ApolloServer GraphQL Server object using type definitions, resolvers, and context objects
 const server = new ApolloServer({
   typeDefs,
   resolvers,

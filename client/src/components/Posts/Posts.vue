2c7d91f7b4dd11c19a2013e068a26b4181222b52
Lucas <33850575+saechaol@users.noreply.github.com>
2021-02-10 16:31:10 -0800

    Implement merge posts function for infinite scroll

--- client/src/components/Posts/Posts.vue
+++ client/src/components/Posts/Posts.vue
@@ -1,11 +1,62 @@
 <template>
-  <v-container>
-    <h1>Posts</h1>
+  <v-container text-center v-if="infiniteScrollPosts">
+    <div v-for="post in infiniteScrollPosts.posts" :key="post._id">
+      <img :src="post.imageUrl" height="100px" alt="" />
+      <h3>{{ post.title }}</h3>
+    </div>
+    <v-btn @click="showMorePosts" v-if="showMoreEnabled">More Posts</v-btn>
   </v-container>
 </template>
 
 <script>
+import { INFINITE_SCROLL_POSTS } from "../../queries";
+
+const pageSize = 2;
+
 export default {
   name: "Posts",
+  data() {
+    return {
+      pageNum: 1,
+      showMoreEnabled: true,
+    };
+  },
+  apollo: {
+    infiniteScrollPosts: {
+      query: INFINITE_SCROLL_POSTS,
+      variables: {
+        pageNum: 1,
+        pageSize,
+      },
+    },
+  },
+  methods: {
+    showMorePosts() {
+      this.pageNum += 1;
+
+      // fetch more posts and transform original result
+      this.$apollo.queries.infiniteScrollPosts.fetchMore({
+        variables: {
+          // page num is being incremented be one
+          pageNum: this.pageNum,
+          pageSize,
+        },
+        updateQuery: (prevResult, { fetchMoreResult }) => {
+          const newPosts = fetchMoreResult.infiniteScrollPosts.posts;
+          const hasMore = fetchMoreResult.infiniteScrollPosts.hasMore;
+          this.showMoreEnabled = hasMore;
+
+          return {
+            infiniteScrollPosts: {
+              __typename: prevResult.infiniteScrollPosts.__typename,
+              // merge previous posts with new posts
+              posts: [...prevResult.infiniteScrollPosts.posts, ...newPosts],
+              hasMore,
+            },
+          };
+        },
+      });
+    },
+  },
 };
 </script>
